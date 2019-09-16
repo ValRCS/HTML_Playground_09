6a92b840166a03ce6bf919a0225072e307ffd838
Valdis Saules <valdis.s.coding@gmail.com>
2019-09-16 12:03:00 +0300

    Added example of string manipulation

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,19 @@
 console.log("Going to alert something");
+// we use Ctrl-/ to comment and uncomment
 // alert("Hello RCS!");
-console.log("I have alerted, lets finish this");
-document.body.style.backgroundColor = "lightblue";
+// console.log("I have alerted, lets finish this");
+// document.body.style.backgroundColor = "lightblue";
+var a = 20;
+var b = 30 - a;
+var c = b * a;
+console.log(a,b,c);
+
+var myDiv = document.getElementById('output');
+//Javascripts lets me add strings with numbers, 
+// meaning JS converts number to sting then concatanetes two strings
+myDiv.innerText = "MyVariables:" + a + " : " + b + " : " + c;
+
+var myName = "Valdis";
+var myStr = "My name is " + myName + " and I am well!";
+
+myDiv.innerText = myStr;
