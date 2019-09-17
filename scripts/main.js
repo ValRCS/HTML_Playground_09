1c08dabaea5d2687ce9afc6375702b344bd68dd2
Valdis Saules <valdis.s.coding@gmail.com>
2019-09-17 10:25:29 +0300

    How to modify innerHTML

--- scripts/main.js
+++ scripts/main.js
@@ -27,8 +27,19 @@ console.log("Going to alert something");
 var a = 20;
 var b = 30 - a;
 var c = b * a;
+
+var myList = [
+      ["Chocolate Bar", 15, "Buy"],
+      ["Chocolate Bar", 10, "Eat"],
+      ["Bread", 15, "Make"],
+      ["Chocolate Bar", 15, "Buy", "SomethingElse"],
+      ["Milk", 2, "Drink"]
+  
+  
+  ];
 console.log(a,b,c);
 
+//  one way of obtaining element by ID
 var myDiv = document.getElementById('output');
 //Javascripts lets me add strings with numbers, 
 // meaning JS converts number to sting then concatanetes two strings
@@ -38,3 +49,12 @@ var myName = "Valdis";
 var myStr = "My name is " + myName + " and I am well!";
 
 myDiv.innerText = wordBlanks( "cat", "heavy", "chased", "mice");
+
+//  second way of obtaining element using query
+var resDiv = document.querySelector('#results')
+
+// quick and dirty way of modifying HTML inside the element
+// for more complicated HTML better to add elements discretely
+resDiv.innerHTML = JSON.stringify(myList);
+
+resDiv.innerHTML += "<hr>Today's Date:" + Date.now();
