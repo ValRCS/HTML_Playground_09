cd0b8fa30d749bc31ca54900d060dd75707fce9f
Valdis Saules <valdis.s.coding@gmail.com>
2019-09-17 11:14:26 +0300

    Make main function and run it

--- scripts/main.js
+++ scripts/main.js
@@ -19,42 +19,50 @@ function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
   
   }
 
-console.log("Going to alert something");
-// we use Ctrl-/ to comment and uncomment
-// alert("Hello RCS!");
-// console.log("I have alerted, lets finish this");
-// document.body.style.backgroundColor = "lightblue";
-var a = 20;
-var b = 30 - a;
-var c = b * a;
+function main() {
+      console.log("Going to alert something");
+      // we use Ctrl-/ to comment and uncomment
+      // alert("Hello RCS!");
+      // console.log("I have alerted, lets finish this");
+      // document.body.style.backgroundColor = "lightblue";
+      var a = 20;
+      var b = 30 - a;
+      var c = b * a;
 
-var myList = [
-      ["Chocolate Bar", 15, "Buy"],
-      ["Chocolate Bar", 10, "Eat"],
-      ["Bread", 15, "Make"],
-      ["Chocolate Bar", 15, "Buy", "SomethingElse"],
-      ["Milk", 2, "Drink"]
-  
-  
-  ];
-console.log(a,b,c);
+      var myList = [
+            ["Chocolate Bar", 15, "Buy"],
+            ["Chocolate Bar", 10, "Eat"],
+            ["Bread", 15, "Make"],
+            ["Chocolate Bar", 15, "Buy", "SomethingElse"],
+            ["Milk", 2, "Drink"]
+      
+      
+      ];
+      console.log(a,b,c);
 
-//  one way of obtaining element by ID
-var myDiv = document.getElementById('output');
-//Javascripts lets me add strings with numbers, 
-// meaning JS converts number to sting then concatanetes two strings
-myDiv.innerText = "MyVariables:" + a + " : " + b + " : " + c;
+      //  one way of obtaining element by ID
+      var myDiv = document.getElementById('output');
+      //Javascripts lets me add strings with numbers, 
+      // meaning JS converts number to sting then concatanetes two strings
+      myDiv.innerText = "MyVariables:" + a + " : " + b + " : " + c;
 
-var myName = "Valdis";
-var myStr = "My name is " + myName + " and I am well!";
+      var myName = "Valdis";
+      var myStr = "My name is " + myName + " and I am well!";
 
-myDiv.innerText = wordBlanks( "cat", "heavy", "chased", "mice");
+      myDiv.innerText = wordBlanks( "cat", "heavy", "chased", "mice");
 
-//  second way of obtaining element using query
-var resDiv = document.querySelector('#results')
+      //  second way of obtaining element using query
+      var resDiv = document.querySelector('#results')
 
-// quick and dirty way of modifying HTML inside the element
-// for more complicated HTML better to add elements discretely
-resDiv.innerHTML = JSON.stringify(myList);
+      // quick and dirty way of modifying HTML inside the element
+      // for more complicated HTML better to add elements discretely
+      resDiv.innerHTML = JSON.stringify(myList);
 
-resDiv.innerHTML += "<hr>Today's Date:" + Date.now();
+      resDiv.innerHTML += "<hr>Today's Date:" + Date.now();
+}
+
+//This will get executed when we load our page
+console.log("Going to run main function");
+main();
+
+
