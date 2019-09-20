b1631f6a1cf617cc11568f8b3454abb69b5625b0
Valdis Saules <valdis.s.coding@gmail.com>
2019-09-20 12:12:03 +0300

    obj manipulation

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,33 @@ const myStorage = {
       }
     };
 
+const contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
     // Your code below this line
     let result = "My " + myAdjective;
