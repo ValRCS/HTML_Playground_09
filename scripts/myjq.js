4d4aabec6a3e923ca3c4f57e1cae2e0d3f92fb74
Valdis Saules <valdis.s.coding@gmail.com>
2019-09-24 11:56:43 +0300

    document ready

--- scripts/myjq.js
+++ scripts/myjq.js
@@ -1,2 +1,15 @@
-$("h1").css("background-color", "green");
-$(".my-txt").css("background-color", "aqua");
+//run main only when document is ready
+$(document).ready(main);
+
+//in a modern browser it would be something like this
+//document.addEventListener('DOMContentLoaded', main, false);
+//but jQuery also supports older browsers with onload etc handlers
+
+function main() {
+    console.log("Running Main!");
+    $("h1").css("background-color", "green");
+    $(".my-txt").css("background-color", "aqua");
+    
+}
+
+
