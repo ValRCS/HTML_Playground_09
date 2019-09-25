1b347e040af251b27ba0dee30d46f9edd4bbd673
Valdis Saules <valdis.s.coding@gmail.com>
2019-09-25 11:32:46 +0300

    this in objects and arrow functions

--- scripts/myjq.js
+++ scripts/myjq.js
@@ -5,10 +5,73 @@ $(document).ready(main);
 //document.addEventListener('DOMContentLoaded', main, false);
 //but jQuery also supports older browsers with onload etc handlers
 
+//do not do this at home!! 
+//you should not add your own variables to global window object! 
+window.a = 10;
+window.b = 20;
+window.total = 100;
+//so do not use functions which call this outside their bound scope
 
+let user = {
+    name: "Valdis",
+    age: 30,
+    //shorthand for adding function object
+    //same as
+    //sayHi : function() { //do stuff}
+    sayHi() {
+      // "this" is the "current object"
+        //   alert(this.name);
+        console.log("My Name is ", this.name);
+        //Without this is not as safe as user could have been changed
+        console.log("My Name is ", user.name);
 
+        // arrow function do not have their own this, they use their parents this
+        const arrfun = () => {
+            console.log("My a is:", this.a);
+        }
+        arrfun();
+    },
+
+    a : 3333
+  
+  };
+  
+  user.sayHi(); // Should see valdis
+
+const myobj = {
+    a : 2,
+    b : 3,
+    total : undefined,
+    //functions are first class citizens we can pass them and assign them
+    add : adder,
+    a2 : add2,
+
+    a3 : function(n) {
+        this.total += n;
+    }
+
+}
+
+const o2 = {
+    a : 3,
+    b : 5,
+    total : 20,
+    oadd : adder
+}
+
+function adder(c) {
+    this.total = this.a + this.b + c;
+    console.log("Total is", this.total);
+}
+
+function add2(i,j) {
+    return i+j;
+}
 
 function main() {
+
+
+
     console.log("Running Main!");
     $("h1").css("background-color", "green");
     $(".my-txt").css("background-color", "aqua");
@@ -33,4 +96,18 @@ function main() {
         console.log("Testing this", this.id);
     });
 
+    myobj.add(20);
+    o2.oadd(50);
+    adder(100);
 }
+
+
+main.a = 100;
+main.b = 200;
+main.total = 0;
+main.add = adder;
+main.add(23);
+
+let a = [6,9];
+a.b = 10;
+console.log(a, a.b);
