67379f712007ddcf887c5f2781ea203030243e6f
Valdis Saules <valdis.s.coding@gmail.com>
2019-09-30 10:51:09 +0300

    add and clear elements

--- scripts/addelements.js
+++ scripts/addelements.js
@@ -7,9 +7,39 @@ addBtn.onclick = (event) => {
     console.log("Going to add this many elements: "+ inputField.value);
 
     //add elements
-    
+    const mainCont = document.querySelector("#main-cont-1");
+    for (let i = 0; i < inputField.value ; i++) {
+        addElement(mainCont, "p", {
+            content: "Lorem",
+            id: "el-"+i,
+            myClasses: ['red-txt', 'my-txt']      
+     })
+    }
+}
+
+const delBtn = document.querySelector(".del-elements");
+//main difference between regular function and arrows is the scope of this
+delBtn.onclick = function (event) {
+    deleteAllElements(document.querySelector("#main-cont-1"));
 }
 
 function addElement(parent, tag, props) {
     console.log("Adding a new element");
+    const newEl = document.createElement(tag);
+    newEl.innerText = props.content;
+    newEl.id = props.id;
+    if (props.hasOwnProperty('myClasses')) {
+        //new ES6 spread operator destructor for our array into parameters
+        //so [1,"foo", "bar"] becomes 1, "foo", "bar"
+        newEl.classList.add(...props.myClasses);
+    }
+    parent.appendChild(newEl);
+
+}
+
+function deleteAllElements(parent) {
+    console.log("Deleting all children");
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+      }
 }
